237069a63c6df4e24504856d19e2518310458f17
Garrett Head <garretthead@Garretts-MacBook-Pro.local>
2025-01-18 10:30:24 -0600

    update App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import Wordle from "./features/Wordle.jsx";
 import { WordleProvider } from "./context/WordleContext.js";
-function App() {
+
+const App = () => {
   return (
     <WordleProvider>
       <div className="App contianer bg-site bg-no-repeat bg-cover overflow-hidden h-svh">
