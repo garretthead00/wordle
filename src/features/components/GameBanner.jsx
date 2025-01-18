3469cb450a43f46f9329b6d595c740b316737cb1
Garrett Head <garretthead@Garretts-MacBook-Pro.local>
2025-01-18 15:27:47 -0600

    Update banner for losing case

--- src/features/components/GameBanner.jsx
+++ src/features/components/GameBanner.jsx
@@ -5,26 +5,23 @@ import { useWordle } from "../../context/WordleContext";
 export const GameBanner = () => {
   const { gameStatus, solution, resetGame } = useWordle();
   return (
-    <>
+    <div className="flex flex-col justify-center container">
       {gameStatus === GAME_STATUS.WON && (
         <div className="game-banner flex flex-col gap-2 justify-center items-center mt-4 text-appGold font-primary text-xl p-4 text-center">
-          <button className="closeButton absolute top-2 right-2">X</button>
-          {gameStatus === GAME_STATUS.WON
-            ? `Congratulations! You guessed the word!`
-            : `Game Over! The word was ${solution}.`}
-          <button
-            className="play-again-button button border bg-appGold text-appPurple text-2xl p-2 rounded-md"
-            onClick={() => resetGame()}
-          >
-            Play Again?
-          </button>
+          {`Congratulations! You guessed the word!`}
         </div>
       )}
       {gameStatus === GAME_STATUS.LOST && (
-        <p className="game-banner flex justify-center items-center mt-2 text-appGold font-primary text-xl p-4 text-center">
-          {`Game Over! The word was ${solution}.`}
-        </p>
+        <div className="game-banner flex flex-col gap-2 justify-center items-center mt-4 text-appGold font-primary text-xl p-4 text-center">
+          {`Sorry, Game Over!`}
+        </div>
       )}
-    </>
+      <button
+        className="play-again-button button border bg-appGold text-appPurple text-2xl p-2 rounded-md"
+        onClick={() => resetGame()}
+      >
+        Play Again?
+      </button>
+    </div>
   );
 };
